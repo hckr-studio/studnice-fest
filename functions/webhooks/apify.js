e86b34eddb345fa45d72dac8f40ae20bee81c9c1
Aleš Roubíček <ales@roubicek.name>
2024-06-20 14:08:19 +0200

    Read scrape data in the web hook

--- functions/webhooks/apify.js
+++ functions/webhooks/apify.js
@@ -1,5 +1,8 @@
 export async function onRequestPost({ env, request, data }) {
   const body = await request.json();
-  console.log({ env, request, body, data })
+  const {defaultDatasetId} = body.resource;
+  const resp = await fetch(`https://api.apify.com/v2/datasets/${defaultDatasetId}/items`);
+  const items = await resp.json();
+  console.log({ items, defaultDatasetId });
   return new Response(null, { status: 200 });
 }
