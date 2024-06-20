6ec37f3541a5524e5cfe12a4e130ef40ee5ea6c6
Aleš Roubíček <ales@roubicek.name>
2024-06-20 14:18:51 +0200

    Use apify token secret to read from dataset

--- functions/webhooks/apify.js
+++ functions/webhooks/apify.js
@@ -1,7 +1,11 @@
 export async function onRequestPost({ env, request, data }) {
   const body = await request.json();
   const {defaultDatasetId} = body.resource;
-  const resp = await fetch(`https://api.apify.com/v2/datasets/${defaultDatasetId}/items`);
+  const resp = await fetch(`https://api.apify.com/v2/datasets/${defaultDatasetId}/items`, {
+    headers: {
+      "Authorization": env.APIFY_TOKEN
+    }
+  });
   const items = await resp.json();
   console.log({ items, defaultDatasetId });
   return new Response(null, { status: 200 });
