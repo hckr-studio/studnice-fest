aeeee8dd0f3e1da7690fc8e47f2c59a916cd4c68
Aleš Roubíček <ales@roubicek.name>
2024-06-20 14:47:53 +0200

    Store scraped FB items

--- functions/webhooks/apify.js
+++ functions/webhooks/apify.js
@@ -1,12 +1,20 @@
 export async function onRequestPost({ env, request, data }) {
   const body = await request.json();
-  const {defaultDatasetId} = body.resource;
+  const { defaultDatasetId } = body.resource;
   const resp = await fetch(`https://api.apify.com/v2/datasets/${defaultDatasetId}/items`, {
-    headers: {
-      "Authorization": env.APIFY_TOKEN
-    }
+    headers: { "Authorization": env.APIFY_TOKEN }
   });
-  const items = await resp.json();
-  console.log({ items, defaultDatasetId });
+  const items = (await resp.json()).filter(x => x.text);
+  const news = items.map(({ timestamp, postId }) => ({ timestamp, postId }));
+  const xx = (await env.NEWS.get("/news", "json")) ?? [];
+  const allNews = new Map(xx.map(({ timestamp, postId }) => [postId, { timestamp, postId }]));
+  for (const { timestamp, postId } of news) {
+    allNews.set(postId, { timestamp, postId });
+  }
+  const xxx = Array.from(allNews.values()).sort((a, b) => b.timestamp - a.timestamp);
+  await env.NEWS.put("/news", JSON.stringify(xxx));
+  for (const item of items) {
+    await env.NEWS.put(`/news/${item.postId}`, JSON.stringify(item));
+  }
   return new Response(null, { status: 200 });
 }
