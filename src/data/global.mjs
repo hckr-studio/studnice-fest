c88c3f781a29b8b3dd0ce021c7e907c9c61af2b1
Aleš Roubíček <ales@roubicek.name>
2024-12-11 11:25:41 +0100

    Hero CTA

--- src/data/global.mjs
+++ src/data/global.mjs
@@ -10,6 +10,7 @@ export default {
   hero: {
     heading: "",
     text: "19. ročník festivalu",
+    cta: "Koupit si vstupenky",
     footer: "#VidimeSeVLese",
     "bg-big": "hero-bg-big.jpg",
     "bg-medium": "hero-bg-big.jpg",
