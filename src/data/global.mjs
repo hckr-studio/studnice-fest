8c97d27177abb1ac2885164d30253309ffd059df
pavel-kosina <pav.kosina@gmail.com>
2025-07-29 22:49:21 +0200

    Update global.mjs

--- src/data/global.mjs
+++ src/data/global.mjs
@@ -24,7 +24,7 @@ export default {
   },
   tickets: {
     url: "https://tickets.nfctron.com/event/eupathia/studnice-fest-2026",
-    currentWave: 1, //"onsite",
+    currentWave: 2, //"onsite",
     waves: [
       ["onsite", [{ days: 1, price: 990 }, { days: 2, price: 1190 }]],
       [6, [
