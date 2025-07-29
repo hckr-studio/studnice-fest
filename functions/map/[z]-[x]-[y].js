93f6cace7850b9bd8f95e9cf7f91f06f2da371b0
Aleš Roubíček <ales@roubicek.name>
2025-07-29 10:57:09 +0200

    push request headers to maps api

--- functions/map/[z]-[x]-[y].js
+++ functions/map/[z]-[x]-[y].js
@@ -2,9 +2,9 @@
  * @param {EventContext<Env>} context
  * @returns {Promise<Response>}
  */
-export async function onRequestGet({ params }) {
+export async function onRequestGet({ params, request }) {
   const API_KEY = "S9n6dfE_8AfjsNvRxSwAmFV4-1z5tfk5W7IUajp2D3I";
   const { z, x, y } = params;
   const url = `https://api.mapy.cz/v1/maptiles/outdoor/256/${z}/${x}/${y}?apikey=${API_KEY}`;
-  return fetch(url);
+  return fetch(url, request);
 }
