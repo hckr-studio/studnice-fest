a2b2fd95eaff1d4439e266de4a3f5dffef9b54d9
Aleš Roubíček <ales@roubicek.name>
2026-04-01 12:32:00 +0200

    Map tiles cache
    Cache map tile for one day, so they are current but don't consume too much credits.

--- functions/map/[z]-[x]-[y].js
+++ functions/map/[z]-[x]-[y].js
@@ -2,9 +2,25 @@
  * @param {EventContext<Env>} context
  * @returns {Promise<Response>}
  */
-export async function onRequestGet({ params, request }) {
+export async function onRequestGet({ params, request, env }) {
   const API_KEY = "S9n6dfE_8AfjsNvRxSwAmFV4-1z5tfk5W7IUajp2D3I";
   const { z, x, y } = params;
-  const url = `https://api.mapy.cz/v1/maptiles/outdoor/256/${z}/${x}/${y}?apikey=${API_KEY}`;
-  return fetch(url, request);
+  const key = `maptiles/outdoor/256/${z}/${x}/${y}`;
+
+  const { value, metadata } = await env.MAPTILES_CACHE.getWithMetadata(key, "stream");
+  if (value) {
+    return new Response(value, metadata);
+  }
+
+  const url = `https://api.mapy.cz/v1/${key}?apikey=${API_KEY}`;
+  const resp = await fetch(url, request);
+  const result = resp.clone(); // keep the response unconsumed for return, because cache will consume the body stream
+  await env.MAPTILES_CACHE.put(key, resp.body, {
+    expirationTtl: 60 * 60 * 24, // one day
+    metadata: {
+      status: resp.status,
+      headers: Array.from(resp.headers.entries())
+    }
+  });
+  return result;
 }
--- functions/worker-configuration.d.ts
+++ functions/worker-configuration.d.ts
@@ -1,5 +1,6 @@
 export interface Env {
   NEWS: KVNamespace;
+  MAPTILES_CACHE: KVNamespace;
   APIFY_TOKEN: string;
   APIFY_WEBHOOK_SECRET: string;
   DEPLOY_HOOK: string;
