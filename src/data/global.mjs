2381ba6561ef22e6f4abb42116339656816f1c04
pavel-kosina <pav.kosina@gmail.com>
2026-06-04 00:17:59 +0200

    Update global.mjs

--- src/data/global.mjs
+++ src/data/global.mjs
@@ -55,11 +55,13 @@ parking: { days: 2, price: 200, id: "ticket-group-7e605dcc-ee61-4f10-a41c-3261d1
       "prago-union",
       "skyline",
       "elektrick-mann",
+      "the-stylists",
       "nikola-mucha",
       "xavier-baumaxa",
       "lazer-viking",
       "dukla-vozovna",
-      "mr-moss"
+      "mr-moss",
+      "jezek-v-deci"
     ],
     stages: {
       main: "Hlavní stage",
