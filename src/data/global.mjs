ea10929bbe8aa1e52a1acbb37b6bcac3c4a4e5c9
pavel-kosina <pav.kosina@gmail.com>
2026-07-17 13:58:11 +0200

    Update global.mjs

--- src/data/global.mjs
+++ src/data/global.mjs
@@ -17,7 +17,7 @@ export default {
     "bg-small": "hero-bg-small.avif"
   },
   info: {
-    visible: false,
+    visible: true,
   },
   newsList: {
     exclude: ["https://www.facebook.com/100063521171808/posts/1029078449219518"]
